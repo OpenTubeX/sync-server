eebfb2fbeaa1db774e43f0b5b1f800a8628ccb36
D3SOX <nico@d3sox.me>
2026-07-21 18:36:04 +0200

    fix(sync): migrate encrypted collection schema

---
+++ migrations/2026-07-21-180000-0000_encrypted_sync_collections/down.sql
@@ -0,0 +1,3 @@
+DROP TABLE encrypted_sync;
+
+ALTER TABLE encrypted_sync_single_document RENAME TO encrypted_sync;
--- migrations/2026-07-21-180000-0000_encrypted_sync_collections/up.sql
+++ migrations/2026-07-21-180000-0000_encrypted_sync_collections/up.sql
@@ -1,6 +1,10 @@
+ALTER TABLE encrypted_sync RENAME TO encrypted_sync_single_document;
+
 CREATE TABLE encrypted_sync(
-    account_id VARCHAR NOT NULL PRIMARY KEY,
+    account_id VARCHAR NOT NULL,
+    collection VARCHAR NOT NULL,
     revision BIGINT NOT NULL,
     payload TEXT NOT NULL,
+    PRIMARY KEY(account_id, collection),
     CONSTRAINT FK__encrypted_sync__account FOREIGN KEY(account_id) REFERENCES account(id) ON DELETE CASCADE
 );
