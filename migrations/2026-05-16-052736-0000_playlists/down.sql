996fbb497fb170d92d124bf4fb8a565430189739
Bnyro <bnyro@tutanota.com>
2026-07-30 16:22:29 +0200

    fix(postgres): fails to start because quoting backticks are not supported

--- migrations/2026-05-16-052736-0000_playlists/down.sql
+++ migrations/2026-05-16-052736-0000_playlists/down.sql
@@ -1,3 +1,3 @@
 -- This file should undo anything in `up.sql`
-DROP TABLE IF EXISTS `playlist`;
-DROP TABLE IF EXISTS `playlist_video_member`;
+DROP TABLE IF EXISTS playlist;
+DROP TABLE IF EXISTS playlist_video_member;
